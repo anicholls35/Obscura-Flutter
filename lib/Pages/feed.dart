363388264d799f08791182612860755ddc8e8e6f
Pickles2235 <32930825+Pickles2235@users.noreply.github.com>
2021-01-13 16:34:38 +0000

    Add TODO's
    TODO's added to keep track of what has to be done for feed task

--- lib/Pages/feed.dart
+++ lib/Pages/feed.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+//TODO: Create a component for posts
+//TODO: Format feed page to look like prototype
+//TODO: Create appbar with correct format
+//TODO: Seperate appbar as component - allow uniformity accross pages
+//TODO: Create a bottom bar
+//TODO: Link to navigation page
+
 class Feed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
