b93c4ceb8505e92231e6a55dd554d66e13ebc25e
Pickles2235 <32930825+Pickles2235@users.noreply.github.com>
2021-01-13 12:49:45 +0000

    Edit colours to hex value from prototype

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColour = Colors.red;
-const Color accentColour = Colors.teal;
+const Color primaryColour = Color(0xffd0005d);
+const Color accentColour = Color(0xff2dfdcf);
+const Color greyColour = Color(0xff2b2b2b);
