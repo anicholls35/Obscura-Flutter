2b0f88cb11f02bbd340184a048ea1d867f017240
Pickles2235 <32930825+Pickles2235@users.noreply.github.com>
2021-01-13 13:17:23 +0000

    Created the skeleton app with navigation routes in place

---
+++ lib/Pages/feed.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class feed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: (Text("Feed")),
+      ),
+      body: Center(
+        child: Text("Hello World"),
+      ),
+    );
+  }
+}
