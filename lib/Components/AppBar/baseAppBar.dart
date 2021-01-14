951f483ca30f3e879324a2539e800c9cea485018
anicholls35 <an79@hw.ac.uk>
2021-01-14 18:35:36 +0000

    misc

--- lib/Components/AppBar/baseAppBar.dart
+++ lib/Components/AppBar/baseAppBar.dart
@@ -17,8 +17,7 @@ class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
   final AppBar appBar;
   final List<Widget> widgets;
 
-  const BaseAppBar(
-      {Key key, @required this.title, @required this.appBar, this.widgets})
+  const BaseAppBar({Key key, @required this.title, this.appBar, this.widgets})
       : super(key: key);
 
   @override
