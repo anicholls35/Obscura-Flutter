28f617e786f6652bd516f74efa357fc72ecfbdb2
anicholls35 <32268678+anicholls35@users.noreply.github.com>
2021-01-14 14:46:18 +0000

    Removed a TODO
    Removed the TODO to separate app bar into own component. Forgot to do before the merge.

--- lib/Pages/feed.dart
+++ lib/Pages/feed.dart
@@ -4,7 +4,6 @@ import 'package:obscura/Components/baseAppBar.dart';
 //TODO: Create a component for posts
 //TODO: Format feed page to look like prototype
 //TODO: Create appbar with correct format
-//TODO: Seperate appbar as component - allow uniformity accross pages
 //TODO: Create a bottom bar
 //TODO: Link to navigation page
 
