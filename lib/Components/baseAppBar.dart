d7f8492247e134e50e13cfa05bf19f8cceb0aba3
Pickles2235 <32930825+Pickles2235@users.noreply.github.com>
2021-01-14 14:37:40 +0000

    Added comments to BaseAppBar File

--- lib/Components/baseAppBar.dart
+++ lib/Components/baseAppBar.dart
@@ -2,6 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:obscura/constants.dart';
 
+//This is the base component for an AppBar which you (Dev) will use to pull
+//accross a unified appbar experience to each page you make. It's not finished
+//as more pages need to be completed for that can be done.
+//background colour is already set. You need to pass a title, an appbar widget,
+//and also any actions (named widgets). Actions will be the search button
+//down the line.
+
+//TODO: Allow Dev to pass a profile image to fill the emoty circle for active profile
+
 class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Color backgroundColor = primaryColour;
   final Text title;
