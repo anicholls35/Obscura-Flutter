1d6b27943d93bb56d82dbb0ed095d11307294437
anicholls35 <32268678+anicholls35@users.noreply.github.com>
2021-01-19 15:58:45 +0000

    Update hello_world.dart

--- lib/Pages/hello_world.dart
+++ lib/Pages/hello_world.dart
@@ -3,11 +3,7 @@ import 'package:obscura/Components/AppBar/baseAppBar.dart';
 import 'package:obscura/Components/AppBar/mapWidget.dart';
 import 'package:obscura/Components/AppBar/searchWidget.dart';
 
-//TODO: Create a component for posts
-//TODO: Format feed page to look like prototype
-//TODO: Create appbar with correct format
-//TODO: Create a bottom bar
-//TODO: Link to navigation page
+//TODO: Use this as a base template for your page
 
 class hello_world extends StatelessWidget {
   @override
