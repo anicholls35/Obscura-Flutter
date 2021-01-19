e47e61bf02bda89bae23062036bae4f224e60d0b
anicholls35 <32268678+anicholls35@users.noreply.github.com>
2021-01-19 16:11:22 +0000

    Update hello_world.dart

--- lib/Pages/hello_world.dart
+++ lib/Pages/hello_world.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:obscura/Components/AppBar/baseAppBar.dart';
-import 'package:obscura/Components/AppBar/mapWidget.dart';
-import 'package:obscura/Components/AppBar/searchWidget.dart';
+import 'package:obscura/Global_Components/AppBar/baseAppBar.dart';
+import 'package:obscura/Global_Components/AppBar/mapWidget.dart';
+import 'package:obscura/Global_Components/AppBar/searchWidget.dart';
 
 //TODO: Use this as a base template for your page
 
