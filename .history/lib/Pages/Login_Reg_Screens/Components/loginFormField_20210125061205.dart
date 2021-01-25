f3e36c793e85de36a9411622d3be3309387acfb5
anicholls35 <an79@hw.ac.uk>
2021-01-25 06:46:34 +0000

    login + reg done

---
+++ .history/lib/Pages/Login_Reg_Screens/Components/loginFormField_20210125061205.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:obscura/constants.dart';
+
+class LoginFormField extends StatelessWidget {
+  //Fields
+  final String formText;
+  final Icon prefixIcon;
+  final EdgeInsetsGeometry edgeInsetsGeometry;
+  final EdgeInsetsGeometry paddingInsets;
+  final Color fillColor;
+  final bool obscure;
+  final TextStyle textStyle;
+
+  //Constructor
+  const LoginFormField({
+    Key key,
+    @required this.formText,
+    this.prefixIcon,
+    @required this.textStyle,
+    @required this.edgeInsetsGeometry,
+    @required this.paddingInsets,
+    @required this.fillColor,
+    @required this.obscure,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color iconColor = Colors.white;
+
+    return Padding(
+        padding: this.paddingInsets,
+        child: TextField(
+          autocorrect: false,
+          obscureText: this.obscure,
+          decoration: InputDecoration(
+            contentPadding: this.edgeInsetsGeometry,
+            hintText: this.formText,
+            prefixIcon: prefixIcon == null ? null : IconTheme(data: IconThemeData(color: iconColor), child: prefixIcon),
+            hintStyle: this.textStyle,
+            filled: true,
+            fillColor: this.fillColor,
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: secondryColour,
+              ),
+            ),
+          ),
+        ));
+  }
+}
