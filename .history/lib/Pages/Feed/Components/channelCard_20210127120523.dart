e2e80b86ef33e1f981e39fba6656e211772e9f9c
anicholls35 <an79@hw.ac.uk>
2021-01-27 12:15:05 +0000

    Complerted
    Made channel cards reactive + Locked app in portait mode.

---
+++ .history/lib/Pages/Feed/Components/channelCard_20210127120523.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:obscura/constants.dart';
+
+class ChannelCard extends StatelessWidget {
+  final String channelName;
+  final NetworkImage channelImage;
+  final double borderRadius = 15;
+  final double width = 150;
+  final double height = 170;
+
+  const ChannelCard({
+    Key key,
+    @required this.channelName,
+    @required this.channelImage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.only(right: 20),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+        elevation: 3,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: width,
+              height: height,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: channelImage,
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: primaryColour.withOpacity(0.7),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(borderRadius),
+                    bottomRight: Radius.circular(borderRadius),
+                  ),
+                ),
+                width: width,
+                height: 40,
+                child: Align(
+                  child: Text(
+                    "$channelName",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ .history/lib/main_20210127121227.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:obscura/Pages/Splash_Screen/splashScreen.dart';
+import 'package:obscura/constants.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(Obscura()));
+}
+
+/// main.dart is responsible for starting the app and dealing with Obscura's
+/// navigation. Pushing and Popping screens as needed.
+
+class Obscura extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Obscura',
+      theme: ThemeData(
+        primaryColor: primaryColour,
+        accentColor: accentColour,
+        scaffoldBackgroundColor: primaryColour,
+      ),
+      routes: {
+        '/': (context) => SplashScreen(),
+      },
+    );
+  }
+}
