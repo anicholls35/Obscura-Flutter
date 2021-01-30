1f8f97b4a690dde9ce15e73f5fdd710e2995f3c7
MagnificentBubble <47347755+MagnificentBubble@users.noreply.github.com>
2021-01-30 15:08:17 +0000

    Moved old button to depricated
    Cleaned old versions ahead of rewrite

--- lib/Pages/Channel_Select/ChannelSelect.dart
+++ lib/Pages/Channel_Select/ChannelSelect.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:obscura/Pages/Channel_Select/Components/ChannelButton.dart';
 import 'package:obscura/constants.dart';
 
 
@@ -21,24 +20,6 @@ class ChannelSelect extends StatelessWidget {
             height: double.infinity,
             color: primaryColour,
           ),
-          Positioned(
-            top: -250,
-            left: -200,
-            child: Container(
-              child: topSvg,
-            ),
-          ),
-          Positioned(
-            bottom: -350,
-            right: -230,
-            child: RotationTransition(
-              turns: AlwaysStoppedAnimation(145 / 260),
-              child: Container(
-                child: bottomSvg,
-              ),
-            ),
-          ),
-          ChannelButton(channelName: "Zoomie", channelImage: NetworkImage("https://www.lolwot.com/wp-content/uploads/2015/12/10-of-the-most-awe-inspiring-landscapes-in-the-world-7.jpg"), subscribers: 2, edgeInsetsGeometry: EdgeInsets.all(10), paddingInsets: EdgeInsets.all(10)),
         ],
       ),
     );
