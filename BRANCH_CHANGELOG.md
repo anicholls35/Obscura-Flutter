d9988f47b3d8387b4ebc3759e672fca4a576207b
anicholls35 <32268678+anicholls35@users.noreply.github.com>
2021-02-12 17:26:38 +0000

    Created Setting choice card widget

--- BRANCH_CHANGELOG.md
+++ BRANCH_CHANGELOG.md
@@ -6,4 +6,6 @@
 3. Allowed User Profile to be a back button in AppBar
 4. Enabled Settings page route in nav bar
 5. Changed Camera Icon on nav bar
+6. Created setting choice card widget
+7. Allow navigation page to be passed to setting choice widget for navigation
 
--- lib/Pages/Settings_Page/settings.dart
+++ lib/Pages/Settings_Page/settings.dart
@@ -4,6 +4,8 @@ import '../../Global_Componets/AppBar/baseAppBar.dart';
 import '../../Global_Componets/Dummy_Assets/fakeData.dart';
 import '../../Global_Componets/NavBar/baseNavBar.dart';
 import '../../constants.dart';
+import '../../constants.dart';
+import '../Feed/feed.dart';
 
 class Settings extends StatelessWidget {
   @override
@@ -11,68 +13,115 @@ class Settings extends StatelessWidget {
     int _pageIndex = 4;
     String versionNumber = '1.1.0-pre+12382';
     return Scaffold(
-        backgroundColor: accentColour,
-        appBar: BaseAppBar(
-          title: Text(''),
-          appBar: AppBar(),
-          activeUserPic: users[0].profilePicture,
-          backgroundColor: accentColour,
-          elevation: 0,
-          backIcon: true,
-        ),
-        bottomNavigationBar: BaseNavBar(index: _pageIndex),
-        body: Stack(
-          children: <Widget>[
-            Positioned(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                //Settings for back panel
-                child: Container(
-                  height: MediaQuery.of(context).size.height * .75,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: primaryColour,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(80),
-                    ),
-                  ),
-                  child: Stack(
-                    //Primary Colour Container Stack - Add Items Here
-                    children: <Widget>[
-                      Positioned(
-                        child: Align(
-                          alignment: Alignment.topRight,
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Text(
-                              'settings'.toUpperCase(),
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 55,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Text(
-                            'VERSION: $versionNumber',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+      backgroundColor: primaryColour,
+      appBar: BaseAppBar(
+        title: Text('settings'.toUpperCase()),
+        appBar: AppBar(),
+        activeUserPic: users[0].profilePicture,
+        elevation: 0,
+        backIcon: true,
+      ),
+      bottomNavigationBar: BaseNavBar(index: _pageIndex),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            child: ListView(
+              children: <Widget>[
+                SettingChoiceCard(
+                  name: 'Account Settings',
+                  icon: Icons.account_circle,
                 ),
+                SettingChoiceCard(
+                  icon: Icons.security,
+                  name: 'Security',
+                ),
+                SettingChoiceCard(
+                  icon: Icons.accessibility_new,
+                  name: 'Accessibility',
+                ),
+                SettingChoiceCard(
+                  icon: Icons.info,
+                  name: 'Policies',
+                ),
+                SettingChoiceCard(
+                  icon: Icons.help,
+                  name: 'About',
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'VERSION: $versionNumber',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w100,
+                    fontSize: 15),
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class SettingChoiceCard extends StatelessWidget {
+  String name;
+  IconData icon;
+  Widget page;
+
+  SettingChoiceCard({
+    Key key,
+    @required this.icon,
+    @required this.name,
+    this.page,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => page));
+        },
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height * 0.1,
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(color: Colors.white, width: 2),
+            ),
+          ),
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  '$name',
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
