7156b58da6737f6bdc0458b00b862e5b8af3b817
anicholls35 <32268678+anicholls35@users.noreply.github.com>
2021-03-30 22:34:04 +0100

    added AlertDialogs for users

--- lib/Pages/Settings_Page/settings.dart
+++ lib/Pages/Settings_Page/settings.dart
@@ -7,15 +7,122 @@ import '../../Global_Componets/Dummy_Assets/fakeData.dart';
 import '../../Global_Componets/NavBar/baseNavBar.dart';
 import '../../Global_Componets/fadeRoute.dart';
 import '../../constants.dart';
-import '../../constants.dart';
 import '../../main.dart';
-import '../Feed/feed.dart';
 
+class SettingChoiceCard extends StatelessWidget {
+  String name;
+  IconData icon;
+  Widget page;
+  bool signOut;
+  void Function() onTap;
+
+  SettingChoiceCard(
+      {Key key,
+      @required this.icon,
+      @required this.name,
+      this.page,
+      this.signOut = false,
+      this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: GestureDetector(
+        onTap: onTap == null
+            ? () {
+                if (this.signOut) {
+                  context.read<AuthenticationService>().signOut().then((res) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text("Signed-Out"),
+                          content: Text(res),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text("Close"),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  });
+                  Navigator.pushReplacement(
+                    context,
+                    FadeRoute(
+                      page: AuthenticationWrapper(),
+                    ),
+                  );
+                } else {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text("Under-Development"),
+                          content: Text(
+                              "Settings is still in development. All options, " +
+                                  "except Sign-out are disabled."),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text("Okay"),
+                            ),
+                          ],
+                        );
+                      });
+                }
+              }
+            : onTap,
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height * 0.1,
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(color: Colors.white, width: 2),
+            ),
+          ),
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  '$name',
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int _pageIndex = 4;
-    String versionNumber = '1.3.1-pre+1238';
+    String versionNumber = '2.1.0-pre+2163';
+    String supportEmail = "luckygroup7@heriotwatt.onmicrosoft.com";
     return Scaffold(
       backgroundColor: primaryColour,
       appBar: BaseAppBar(
@@ -56,6 +163,34 @@ class Settings extends StatelessWidget {
                   name: "Sign Out",
                   signOut: true,
                 ),
+                SettingChoiceCard(
+                  icon: Icons.delete_forever,
+                  name: "Withdraw from Test",
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text("Withdrawl Instructions"),
+                            content: Text(
+                                "To withdrawl from the Obscura Early Development Test, " +
+                                    "please email $supportEmail\nPlease allow " +
+                                    "up to 48 hours for your account " +
+                                    "to be removed from our servers.\n\n" +
+                                    "Please provide feedback via the " +
+                                    "questionaire handed out with the APK"),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text("Okay"),
+                              ),
+                            ],
+                          );
+                        });
+                  },
+                ),
               ],
             ),
           ),
@@ -72,94 +207,6 @@ class Settings extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class SettingChoiceCard extends StatelessWidget {
-  String name;
-  IconData icon;
-  Widget page;
-  bool signOut;
-
-  SettingChoiceCard({
-    Key key,
-    @required this.icon,
-    @required this.name,
-    this.page,
-    this.signOut = false,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-      child: GestureDetector(
-        onTap: () {
-          if (this.signOut) {
-            context.read<AuthenticationService>().signOut().then((res) {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text("Signed-Out"),
-                    content: Text(res),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Text("Close"),
-                      ),
-                    ],
-                  );
-                },
-              );
-            });
-            Navigator.pushReplacement(
-              context,
-              FadeRoute(
-                page: AuthenticationWrapper(),
-              ),
-            );
-          } else {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => page));
-          }
-        },
-        child: Container(
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height * 0.1,
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(color: Colors.white, width: 2),
-            ),
-          ),
-          child: Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Icon(
-                  icon,
-                  color: Colors.white,
-                  size: 40,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Text(
-                  '$name',
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w400),
-                ),
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
